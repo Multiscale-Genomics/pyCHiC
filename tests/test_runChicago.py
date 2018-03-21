7b22c2fe5b77dbb91fbbe2e86669796960a75bc6
Pablo Acera <pacera@ebi.ac.uk>
2018-03-21 15:23:23 +0000

    data for testing runChicago.py included in tests/data

--- tests/test_runChicago.py
+++ tests/test_runChicago.py
@@ -25,44 +25,49 @@ def test_chicago():
     """
     Function to testing the R chicago wrapper runChicago.py
     """
-    input_files = {"chinput_files" : 
-                    "/Users/pacera/MuG/chicagoTeam-chicago-ceffddda8ea3/"+
-                     "PCHiCdata/inst/extdata/GMchinputFiles/GM_rep1.chinput"}
-                    # "/Users/pacera/MuG/chicagoTeam-chicago-ceffddda8ea3/"+
-                    # "PCHiCdata/inst/extdata/GMchinputFiles/GM_rep2.chinput"]
-                 # }
+    #
+    input_files = {
+        "chinput_files":
+            "/Users/pacera/MuG/chicagoTeam-chicago-ceffddda8ea3/"+
+            "PCHiCdata/inst/extdata/GMchinputFiles/GM_rep1.chinput"
+        }
 
-    output_files = { "output_dir" : "/Users/pacera/MuG/chicagoTeam-chicago-ceffddda8ea3/PCHiCdata/output__",
-                    "output_prefix" : "output_test"}
+    output_files = {
+        "output_dir":
+            "/Users/pacera/test_pipeline/mg-process-test1/output/",
+        "output_prefix" : "output_test"
+        }
 
+    metadata = {
+        "chinput_1" : Metadata(
+            "data_chicago", "chinput", [], None, None, 9606),
+        "chinput_2" : Metadata(
+            "data_chicago", "chinput", [], None, None, 9606)
+        }
 
-
-    metadata = { "chinput_1" : Metadata(
-                    "data_chicago", "chinput",[], None,
-                     None, 9606),
-                "chinput_2" : Metadata(
-                    "data_chicago", "chinput",[], None,
-                     None, 9606)
-                }
-
-    config = {"chicago_setting_file": "/Users/pacera/MuG/chicagoTeam-chicago-ceffddda8ea3/PCHiCdata/inst/extdata/sGM12878Settings/sGM12878.settingsFile",
-               "chicago_desing_dir": "/Users/pacera/MuG/chicagoTeam-chicago-ceffddda8ea3/PCHiCdata/inst/extdata/hg19TestDesign",
-               #"chicago_print_memory": None,
-               "chicago_cutoff": "5",
-               "chicago_export_format": "washU_text",
-               #"chicago_export_order": None,
-               #"chicago_rda": None,
-               #"chicago_save_df_only": None,
-               "chicago_examples_prox_dist": "1e6" ,
-               #"chicago_examples_full_range": None,
-               #"chicago_en_feat_files": None,
-               #"chicago_en_feat_folder": None,
-               "chicago_en_min_dist": "0",
-               "chicago_en_max_dist": "1e6",
-               #"chicago_en_full_cis_range": None,
-               "chicago_en_sample_no": "100"}
-               #"chicago_en_trans": None,
-               #"chicago_features_only":None}
+    config = {
+        "chicago_setting_file":
+            "/Users/pacera/test_pipeline/mg-process-test1/tests/"+
+            "data/sGM12878Settings/sGM12878.settingsFile",
+        "chicago_desing_dir":
+            "/Users/pacera/test_pipeline/mg-process-test1/"+
+            "tests/data/hg19TestDesign/",
+        #"chicago_print_memory": None,
+        "chicago_cutoff": "5",
+        "chicago_export_format": "washU_text",
+        #"chicago_export_order": None,
+        #"chicago_rda": None,
+        #"chicago_save_df_only": None,
+        "chicago_examples_prox_dist": "1e6",
+        #"chicago_examples_full_range": None,
+        #"chicago_en_feat_files": None,
+        #"chicago_en_feat_folder": None,
+        "chicago_en_min_dist": "0",
+        "chicago_en_max_dist": "1e6",
+        #"chicago_en_full_cis_range": None,
+        "chicago_en_sample_no": "100"}
+        #"chicago_en_trans": None,
+        #"chicago_features_only":None}
 
 
     chicago_handle = ChicagoTool(config)
