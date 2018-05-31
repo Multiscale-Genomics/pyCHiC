307f9ffb9c7cfc997cee02de6c20b13376c496d7
Pablo <pabloacera91@gmail.com>
2018-05-31 16:41:15 +0100

    change cwd from the bash sript in tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -14,5 +14,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+cd "$(dirname "$0")"
 python test_toolchains.py
