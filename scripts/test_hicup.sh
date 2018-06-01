9c177eb7529eacbf815e60d24a07424df4894099
Pablo <pabloacera91@gmail.com>
2018-06-01 09:53:18 +0100

    test hicup

---
+++ scripts/test_hicup.sh
@@ -0,0 +1,3 @@
+
+
+hicup_truncater /home/travis/build/pabloacera/C-HiC/tests/data/test_truncater/SRR3535023_1.fastq /home/travis/build/pabloacera/C-HiC/tests/data/test_truncater/SRR3535023_2.fastq --outdir /home/travis/build/pabloacera/C-HiC/tests/data/test_truncater/ --quiet --quiet --threads 2 --re1 A^AGCT
