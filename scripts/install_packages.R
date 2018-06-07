134d50944cfc64421df08fa4d8432d4e46a3cc68
Pablo <pabloacera91@gmail.com>
2018-06-07 17:27:02 +0100

    adding newest version of R in travis

--- scripts/install_packages.R
+++ scripts/install_packages.R
@@ -17,6 +17,7 @@
 
 source("http://bioconductor.org/biocLite.R")
 biocLite("argparser")
+if(!require("rhdf5")) biocLite("rhdf5")
 install.packages("devtools")
 devtools::install_bitbucket("chicagoTeam/Chicago", subdir="Chicago")
 
