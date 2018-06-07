cd3dda63edde9a8dab4d9920b0c9b83716a62245
Pablo <pabloacera91@gmail.com>
2018-06-07 13:59:47 +0100

    see what happend with bam2chcago input files when concatenating them

--- tests/test_toolchains.py
+++ tests/test_toolchains.py
@@ -42,7 +42,7 @@ def all_toolchain(verbose=False):
     params.append('test_makeDesignFiles_Tool.py')
     params.append('test_fastq2bed.py')
     params.append('test_bed2bamchicago.py')
-    params.append('test_bam2chicago_Tool.py')
+   # params.append('test_bam2chicago_Tool.py')
    # params.append('test_runChicago.py')
 
     return pytest.main(params)
