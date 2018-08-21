57abf5fe063c5f63567ae59ab211e6465698c1e7
pabloacera <pabloacera91@gmail.com>
2018-08-21 16:00:01 +0100

    change the path to hicup digester to execute directly

--- CHiC/tool/hicup_tool.py
+++ CHiC/tool/hicup_tool.py
@@ -18,7 +18,7 @@ import os
 import subprocess
 import sys
 from utils import logger
-
+from os.path import expanduser
 
 try:
     if hasattr(sys, '_run_from_cmdl') is True:
@@ -205,16 +205,15 @@ class hicup(Tool):
             sonication protocol is not followed. Typically the sonication
             protocol is followed.
         """
-
-
+        home = expanduser("~")
         if re_enzyme2 is True:
-            args = ["hicup_digester",
+            args = [home+"/bin/hicup_digester",
                     "--genome", genome_name,
                     "--re1", re_enzyme,
                     "--re2", re_enzyme2,
                     genome_loc]
         else:
-            args = ["hicup_digester",
+            args = [home+"/bin/hicup_digester",
                     "--genome", genome_name,
                     "--re1", re_enzyme,
                     genome_loc]
