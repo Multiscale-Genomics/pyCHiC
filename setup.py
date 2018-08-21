8f83981926c153c26644f63a7c98e9d9c9f820dc
pabloacera <pabloacera91@gmail.com>
2018-08-21 09:03:54 +0100

    adding pandas travis

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'numpy', 'pytest', 'scipy', 'matplotlib', 'pysam', 'mock',
-        'bz2file', 'ConfigParser', 'rtree', 'pylint'
+        'bz2file', 'ConfigParser', 'rtree', 'pylint', 'pandas'
     ],
     setup_requires=[
         'pytest-runner',
